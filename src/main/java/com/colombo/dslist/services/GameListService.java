d38ccb211229cdaacf908175cc3cec648635c47d
João Vitor Colombo <jvcolombo.06@gmail.com>
2025-08-23 15:44:21 -0300

    feat: replace list

--- src/main/java/com/colombo/dslist/services/GameListService.java
+++ src/main/java/com/colombo/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.colombo.dslist.dto.GameListDTO;
 import com.colombo.dslist.entities.GameList;
+import com.colombo.dslist.projections.GameMinProjection;
 import com.colombo.dslist.repositories.GameListRepository;
+import com.colombo.dslist.repositories.GameRepository;
 
 
 @Service
@@ -17,9 +19,24 @@ public class GameListService {
   @Autowired
   private GameListRepository gameListRepository;
 
+  @Autowired
+  private GameRepository gameRepository;
+
   @Transactional(readOnly= true)
   public List<GameListDTO> findAll() {
     List<GameList> result = gameListRepository.findAll();
     return result.stream().map(x -> new GameListDTO(x)).toList();
+  }
+
+  @Transactional
+  public void move(Long listId, int sourceIndex, int destinationIndex) {
+    List<GameMinProjection> list = gameRepository.searchByList(listId);
+    GameMinProjection obj = list.remove(sourceIndex);
+    list.add(destinationIndex, obj);
+    int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+    int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+    for (int i = min; i <= max; i++) {
+      gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+    }
   }
 }
